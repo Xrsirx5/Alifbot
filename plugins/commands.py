001254943295d788aff4fdf5067e942d6729c6c2
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-10-08 12:11:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ async def start(bot, message):
         await message.reply(INVITE_MSG)
     else:
         buttons = [[
-            InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('😃Search Here😃', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
